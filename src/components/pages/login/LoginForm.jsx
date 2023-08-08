f367b259970cf2083c5325c2c6831e3822414694
RatanaSenpai <createbysenpai@gmail.com>
2023-08-08 10:45:36 +0200

    ♻️ F01: LOGIN > Create LoginForm

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,32 +1,33 @@
-import { useState } from "react"
+import React, { useState } from 'react'
 
-export default function LoginPage() {
+export default function LoginForm() {
     //state
     const [inputValue, setInputValue] = useState("")
+
     //comportement
     const handleSubmit = (e) => {
         e.preventDefault()
         alert(`Bonjour ${inputValue}`)
         setInputValue("")
     }
+
     const handleChange = (e) => {   
             setInputValue(e.target.value)
         }
+
     //affichage (render)
-    return (        
-        <div>
+    return (
+        <form action="submit" onSubmit={handleSubmit}>
             <h1>Bienvenue chez nous !</h1>
             <br />
             <h2>Connectez vous</h2>
-            <form action="submit" onSubmit={handleSubmit}>
-                <input 
-                value={inputValue} 
-                onChange={handleChange} 
-                type="text" 
-                placeholder="Entrez votre prénom..." 
+            <input
+                value={inputValue}
+                onChange={handleChange}
+                type="text"
+                placeholder="Entrez votre prénom..."
                 required />
-                <button>Accédez à votre espace</button>        
-            </form>
-        </div>
+            <button>Accédez à votre espace</button>
+        </form>
     )
 }
