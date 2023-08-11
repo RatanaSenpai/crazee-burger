243310a210b4da2e9feef34e4e88266d4e5a19bb
RatanaSenpai <createbysenpai@gmail.com>
2023-08-11 16:07:04 +0200

    ✨ F02: LOGIN > Add redirection from/to ErrorPage

--- src/components/pages/error/ErrorPage.jsx
+++ src/components/pages/error/ErrorPage.jsx
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function ErrorPage() {
     return (
-        <div>ErrorPage</div>
+        <div>
+            <h1>ErrorPage</h1>
+            <Link to="/">
+                <button>Retourner vers la page d'accueil</button>
+            </Link>
+        </div>
     )
 }
