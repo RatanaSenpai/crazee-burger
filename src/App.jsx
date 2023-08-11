73c6db94ba6ebbbd630313f37359c48e149f0c80
RatanaSenpai <createbysenpai@gmail.com>
2023-08-11 15:36:44 +0200

    ✨ F02: APP > Install routing system

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import "./App.css"
 import LoginPage from "./components/pages/login/LoginPage"
+import { Route, Routes } from "react-router-dom"
+import OrderPage from "./components/pages/order/OrderPage"
+import ErrorPage from "./components/pages/error/ErrorPage"
 
-function App () {
-  return <LoginPage />
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/order" element={<OrderPage />} />
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
+
+  )
 }
 
 export default App
