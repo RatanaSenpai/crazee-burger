0aac656f06433e5b916bd70fddadc15a469ae580
RatanaSenpai <createbysenpai@gmail.com>
2023-08-22 15:42:57 +0200

    💄 F03: LOGIN > Add stylinh and background

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -3,7 +3,6 @@ import Logo from "../../reusable-ui/Logo";
 import LoginForm from "./LoginForm"
 
 export default function LoginPage() {
-    //affichage (render)
     return (        
         <LoginPageStyled>
             <Logo />
@@ -14,10 +13,25 @@ export default function LoginPage() {
 }
 
 const LoginPageStyled = styled.div`
-    background: red;
     height: 100vh;
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
-    flex-direction: column;
+
+    ::before {
+        content: "";
+        background: url("/images/burger-and-fries-background.jpg") rgba(0, 0, 0, 0.7);
+        background-size: cover;
+        background-position: center;
+        background-blend-mode: darken;
+
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        z-index: -1;
+    }
+
 `;
