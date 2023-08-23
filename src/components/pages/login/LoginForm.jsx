6f5f761d65b3d5f965b3c4c8481112231a3167cf
RatanaSenpai <createbysenpai@gmail.com>
2023-08-23 11:06:27 +0200

    💄 F03: LoginForm > Add styling

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -20,7 +20,7 @@ export default function LoginForm() {
         <LoginFormStyled action="submit" onSubmit={handleSubmit}>
             <div>
                 <h1>Bienvenue chez nous !</h1>
-                <br />
+                <hr />
                 <h2>Connectez-vous</h2>
             </div>
             <div>
@@ -38,4 +38,28 @@ export default function LoginForm() {
 
 const  LoginFormStyled = styled.div`
     background: green;
+    text-align: center;
+    max-width: 500px;
+    min-width: 400px;
+    margin: 0px auto;
+    padding: 2.5rem 2rem;
+    border-radius: 5px;
+    font-family: "Amatic SC", cursive;
+
+    hr {
+        border: 1.5px solid #f56a2c;
+        margin-bottom: 40px;
+    }
+
+    h1 {
+        color: white;
+        font-size: 48px;
+    }
+
+    h2 {
+        color: #8e8b8b;
+        margin: 20px 10px 10px;
+        color: white;
+        font-size: 36px;
+    }
 `
