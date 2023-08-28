102dc98f18c7311b50b92d0ede0885fab9591c5b
RatanaSenpai <createbysenpai@gmail.com>
2023-08-28 14:53:11 +0200

    💄 F03: LOGIN > LoginForm > Replace hard coded value with Design System values

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -6,6 +6,7 @@ import { BsPersonCircle } from "react-icons/bs"
 import { IoChevronForwardOutline } from "react-icons/io5"
 import TextInput from '../../reusable-ui/TextInput';
 import PrimaryButton from '../../reusable-ui/PrimaryButton';
+import { theme } from '../../../theme';
 
 
 export default function LoginForm() {
@@ -53,31 +54,30 @@ const  LoginFormStyled = styled.form`
     min-width: 400px;
     margin: 0px auto;
     padding: 2.5rem 2rem;
-    border-radius: 5px;
+    border-radius: ${theme.borderRadius.round};
     font-family: "Amatic SC", cursive;
 
     hr {
-        border: 1.5px solid #f56a2c;
-        margin-bottom: 40px;
+        border: 1.5px solid ${theme.colors.loginLine};
+        margin-bottom: ${theme.gridUnit * 5}px;
     }
 
     h1 {
-        color: white;
-        font-size: 48px;
+        color: ${theme.colors.white};
+        font-size: ${theme.fonts.size.P5};
     }
 
     h2 {
-        color: #8e8b8b;
         margin: 20px 10px 10px;
-        color: white;
-        font-size: 36px;
+        color: ${theme.colors.white};
+        font-size: ${theme.fonts.size.P4};
     }
 
     .icon {
             display: flex;
             justify-content: center;
             align-items: center;
-            font-size: 15px;
+            font-size: ${theme.fonts.size.P0};
             margin-left: 10px;
         }
 `
--- src/components/reusable-ui/TextInput.jsx
+++ src/components/reusable-ui/TextInput.jsx
@@ -1,4 +1,5 @@
 import { styled } from "styled-components";
+import { theme } from "../../theme";
 
 export default function TextInput({ value, onChange, Icon, ...restProps }) {
     return (
@@ -15,27 +16,28 @@ export default function TextInput({ value, onChange, Icon, ...restProps }) {
 
 const InputStyled = styled.div`
         background-color: #fff;
-        border-radius: 5px;
+        border-radius: ${theme.borderRadius.round};
         display: flex;
         align-items: center;
         padding: 18px 24px;
         margin: 18px 0;
 
         .icon {
-            font-size: 15px;
+            font-size: ${theme.fonts.size.P0};
             margin-right: 8px;
-            color: #93a2b1;
+            color: ${theme.colors.greySemiDark};
         }
 
         input {
             border: none;
-            font-size: 15px;
-            color: #17161a;
-            /* width: 100%; */
+            font-size: ${theme.fonts.size.P0};
+            color: ${theme.colors.dark};
+            width: 100%;
+            
+            &::placeholder {
+                background: ${theme.colors.white};
+                color: ${theme.colors.greyMedium};
+            }
         }
 
-        &::placeholder {
-            background: white;
-            color: lightgrey;
-        }
 `;
