774c40cec0128cf5eaed9e59b280b982150e815a
RatanaSenpai <createbysenpai@gmail.com>
2023-08-30 13:49:26 +0200

    🍱 F05: Add assets

--- src/components/pages/order/Main/Menu.jsx
+++ src/components/pages/order/Main/Menu.jsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
 import { styled } from "styled-components";
 
 
 export default function Menu() {
     return (
+
+        const [menu, setmenu] = useState(fakemenu)
+
         <MenuStyled className="menu">Menu</MenuStyled>
     )
 }
