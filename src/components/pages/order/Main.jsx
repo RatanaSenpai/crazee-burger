78909816852296f394b35102cf4a32dea3f22d9c
RatanaSenpai <createbysenpai@gmail.com>
2023-08-30 10:59:44 +0200

    🚧 F05: ORDER > Main > Add grid layout

--- src/components/pages/order/Main.jsx
+++ src/components/pages/order/Main.jsx
@@ -3,7 +3,10 @@ import { theme } from "../../../theme";
 
 export default function Main() {
     return (
-        <MainStyled></MainStyled>
+        <MainStyled>
+            <div className="basket">Basket</div>
+            <div className="menu">Menu</div>
+        </MainStyled>
     )
 }
 
@@ -14,5 +17,16 @@ const MainStyled = styled.div`
     border-bottom-left-radius: ${theme.borderRadius.extraRound};
     border-bottom-right-radius: ${theme.borderRadius.extraRound};
     box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset ;
+
+    display: grid;
+    grid-template-columns: 25% 1fr;
+
+    .basket {
+        background: pink;
+    };
+
+    .menu {
+        background: purple;
+    };
 
 `;
