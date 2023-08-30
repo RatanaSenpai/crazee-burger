feb7767e30f46560b13254d63429a91437ff794e
RatanaSenpai <createbysenpai@gmail.com>
2023-08-30 12:04:12 +0200

    ♻️ F05: ORDER > MAIN > Create Menu

--- src/components/pages/order/Main/Main.jsx
+++ src/components/pages/order/Main/Main.jsx
@@ -1,11 +1,12 @@
 import { styled } from "styled-components";
 import { theme } from "../../../../theme";
+import Menu from "./Menu";
 
 export default function Main() {
     return (
         <MainStyled>
             <div className="basket">Basket</div>
-            <div className="menu">Menu</div>
+            <Menu />
         </MainStyled>
     )
 }
@@ -25,8 +26,6 @@ const MainStyled = styled.div`
         background: pink;
     };
 
-    .menu {
-        background: purple;
-    };
+    
 
 `;
