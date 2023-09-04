4d9982e8e430fc8a2af6b626ec6b93629f794519
RatanaSenpai <createbysenpai@gmail.com>
2023-09-04 11:05:58 +0200

    💄 F05: ORDER > Product > Add image layout

--- src/components/pages/order/Main/Product.jsx
+++ src/components/pages/order/Main/Product.jsx
@@ -8,8 +8,8 @@ export default function Product({ imageSource, title, price }) {
                 <img src={imageSource} alt={title} />
             </div>
             <div className="info-text">
+                <div className="title">{title}</div>
                 <div className="description">
-                    <div className="title">{title}</div>
                     <div className="price">{price}</div>
                     <button className="add-button">Ajouter</button>
                 </div>
@@ -20,22 +20,28 @@ export default function Product({ imageSource, title, price }) {
 
 const ProductStyled = styled.div`
     background: red;
-    width: 240px;
-    height: 330px;
+    width: 200px;
+    height: 300px;
+    display: grid;
+    grid-template-rows: 65% 1fr;
+    padding: 20px;
+    padding-bottom: 10px;
 
     .image {
-        border: 2px solid fuchsia;
-        width: 100px;
+        border: 2px solid yellow;
+        width: 100%;
         height: auto;
+        margin-top: 30px;
         
         img {
             width: 100%;
             height: 100%;
+            object-fit: contain
         }
     }
 
-    .description {
-        border: 2px solid fuchsia;
+    .info-text {
+        border: 2px solid blue;
     }
     
 `;
