4f9689e9d1332d274a98d13cc0916747a22a20b1
RatanaSenpai <createbysenpai@gmail.com>
2023-09-20 15:15:17 +0200

    ♻️ F03: THEME > Update font-size and shadows

--- src/components/reusable-ui/TextInput.jsx
+++ src/components/reusable-ui/TextInput.jsx
@@ -23,14 +23,14 @@ const InputStyled = styled.div`
         margin: 18px 0;
 
         .icon {
-            font-size: ${theme.fonts.size.P0};
+            font-size: ${theme.fonts.size.SM};
             margin-right: 8px;
             color: ${theme.colors.greySemiDark};
         }
 
         input {
             border: none;
-            font-size: ${theme.fonts.size.P0};
+            font-size: ${theme.fonts.size.SM};
             color: ${theme.colors.dark};
             width: 100%;
             
--- src/theme/index.js
+++ src/theme/index.js
@@ -10,7 +10,6 @@ const colors = {
   red: "#e25549",
   redSecondary: "#c4151c",
   blue: "#51a7e1",
-  greyExtraLight: "lightgrey",
   greyLight: "#e4e5e9",
   greyMedium: "#a7a8ad",
   greySemiDark: "#93a2b1",
@@ -34,14 +33,15 @@ const fonts = {
     XXXS: "8px",
     XXS: "10px",
     XS: "12px",
-    P0: "15px",
+    SM: "15px",
+    P0: "16px",
     P1: "18px",
     P2: "20px",
     P3: "24px",
     P4: "36px",
     P5: "48px",
     P6: "60px",
-  }, 
+  },
   weights: {
     light: 300,
     regular: 400,
@@ -49,7 +49,7 @@ const fonts = {
     semiBold: 600,
     bold: 700,
     heavy: 800,
-  }
+  },
 }
 const gridUnit = 8
 const borderRadius = {
@@ -58,8 +58,12 @@ const borderRadius = {
   extraRound: "15px",
   circle: "50%",
 }
-const shadows = {}
 
+const shadows = {
+  subtle: "0px -6px 8px -2px rgba(0, 0, 0, 0.1)",
+  medium: "-8px 8px 20px 0px rgb(0 0 0 / 20%)",
+  strong: "0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset",
+}
 
 export const theme = {
   colors,
