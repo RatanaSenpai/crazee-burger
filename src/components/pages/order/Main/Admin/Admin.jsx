845e4daf025dbf06991958734c9b8aecdea90216
RatanaSenpai <createbysenpai@gmail.com>
2023-09-22 09:47:08 +0200

    💄 F07: ADMIN > Add AdminTabs and AdminPanel

--- src/components/pages/order/Main/Admin/Admin.jsx
+++ src/components/pages/order/Main/Admin/Admin.jsx
@@ -1,19 +1,21 @@
 import styled from "styled-components";
-import { theme } from "../../../../../theme";
+import AdminTabs from "./AdminTabs";
+import AdminPanel from "./AdminPanel";
 
 export default function Admin() {
-    return <AdminStyled>Admin</AdminStyled>
+    return (
+        <AdminStyled>
+            <AdminTabs />
+            <AdminPanel />
+        </AdminStyled>
+    )    
     
 }
 
 const AdminStyled = styled.div`
-    height: 250px;
     position: absolute;
     bottom: 0;
     left: 0;
     right: 0;
-    background: ${theme.colors.white};
-    border: 1px solid ${theme.colors.greyLight};
-    box-shadow: ${theme.shadows.subtle};
-
+    
 `;
---
+++ src/components/pages/order/Main/Admin/AdminTabs.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export default function AdminTabs() {
+    return (
+        <AdminTabsStyled>AdminTabs</AdminTabsStyled>
+    )
+}
+
+const AdminTabsStyled = styled.div`
+    border: 1px solid red;
+`;
