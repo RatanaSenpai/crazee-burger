d52ba07cd89a77efd15ceab396ced2fa444bd33b
RatanaSenpai <createbysenpai@gmail.com>
2023-09-29 12:27:16 +0200

    💄 F07: THEME > Add shadows

--- src/components/pages/order/Main/Main.jsx
+++ src/components/pages/order/Main/Main.jsx
@@ -26,7 +26,7 @@ const MainStyled = styled.div`
 
     border-bottom-left-radius: ${theme.borderRadius.extraRound};
     border-bottom-right-radius: ${theme.borderRadius.extraRound};
-    box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset ;
+    box-shadow: ${theme.shadows.strong};
 
     display: grid;
     grid-template-columns: 1fr;
