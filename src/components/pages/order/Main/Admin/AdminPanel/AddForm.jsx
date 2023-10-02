5796da70ea875c72dca1749e72acf0a16b2ce59b
RatanaSenpai <createbysenpai@gmail.com>
2023-10-02 16:12:35 +0200

    🚧 F08: ADMIN: AddForm >  Add inputs layout

--- src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
+++ src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
@@ -1,6 +1,44 @@
+import styled from "styled-components";
 
 export default function AddForm() {
     return (
-        <div>AddForm</div>
+        <AddFormStyled>
+            <div className="image-preview">Image preview</div>
+            <div className="input-fields">Input fields
+                <input type="text" placeholder="Nom" />
+                <input type="text" placeholder="Image URL" />
+                <input type="text" placeholder="Prix" />
+            </div>
+            <button className="submit-button">Submit button</button>
+        </AddFormStyled>
     )
 }
+
+const AddFormStyled = styled.form`
+    border: 2px solid black;
+    height: 100%;
+    height: 70%;
+
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+    grid-template-rows: repeat(4, 1fr);
+
+    .image-preview {
+        background: red;
+        grid-area: 1 / 1 / 4 / 2;
+    }
+    .input-fields {
+        background: blue;
+        grid-area: 1 / 2 / -2 / 3;
+
+        display: grid;
+    }
+    .submit-button {
+        background: green;
+        grid-area: 4 / -2 / -1 / -1;
+
+        width: 50%;
+    }
+
+    
+`;
--- src/components/pages/order/Main/Admin/AdminPanel/AdminPanel.jsx
+++ src/components/pages/order/Main/Admin/AdminPanel/AdminPanel.jsx
@@ -23,4 +23,5 @@ const AdminPanelStyled = styled.div`
     background: ${theme.colors.white};
     border: 1px solid ${theme.colors.greyLight};
     box-shadow: ${theme.shadows.subtle};
+    padding: 30px 5%;
 `
