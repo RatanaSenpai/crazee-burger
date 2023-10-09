75ee280544777f3d5ee9e935ef972282ac6c51ac
RatanaSenpai <createbysenpai@gmail.com>
2023-10-09 15:19:20 +0200

    ✨ F08: ADMIN: AddForm > Add image preview

--- src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
+++ src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
@@ -35,7 +35,13 @@ export default function AddForm() {
     // affichage
     return (
         <AddFormStyled onSubmit={handleSubmit}>
-            <div className="image-preview">Image preview</div>
+            <div className="image-preview">
+                {newProduct.imageSource ? (
+                    <img src={newProduct.imageSource} alt={newProduct.title} />
+                ) : (
+                    <div>Aucune image</div>
+                )}
+            </div>
             <div className="input-fields">
                 <input  name='title'
                     value={newProduct.title} 
@@ -73,6 +79,16 @@ const AddFormStyled = styled.form`
     .image-preview {
         background: red;
         grid-area: 1 / 1 / 4 / 2;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        img {
+            width: 100%;
+            height: 100%;
+            object-fit: contain;
+            object-position: center;
+        }
     }
     .input-fields {
         background: blue;
