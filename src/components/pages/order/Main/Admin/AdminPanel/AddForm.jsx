c77f6b263363ab9a2ebaeaa0737cff3e5a4c09f9
RatanaSenpai <createbysenpai@gmail.com>
2023-10-18 16:29:17 +0200

    ♻️ F08: AddForm > Extract ImagePreview

--- src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
+++ src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
@@ -8,6 +8,7 @@ import { MdOutlineEuro } from "react-icons/md"
 import { theme } from "../../../../../../theme"
 import TextInput from "../../../../../reusable-ui/TextInput";
 import Button from "../../../../../reusable-ui/Button";
+import ImagePreview from "./ImagePreview";
 
 export const EMPTY_PRODUCT = {
     id: "",
@@ -52,13 +53,7 @@ export default function AddForm() {
     // affichage
     return (
         <AddFormStyled onSubmit={handleSubmit}>
-            <div className="image-preview">
-                {newProduct.imageSource ? (
-                    <img src={newProduct.imageSource} alt={newProduct.title} />
-                ) : (
-                    <div className="empty-image">Aucune Image</div>
-                )}
-            </div>
+            <ImagePreview imageSource={newProduct.imageSource} title={newProduct.title}/>
             <div className="input-fields">
                 <TextInput
                     name="title"
@@ -110,34 +105,6 @@ const AddFormStyled = styled.form`
     width: 70%;
     grid-column-gap: 20px;
     grid-row-gap: 8px;
-
-    .image-preview {
-        /* background: red; */
-        grid-area: 1 / 1 / 4 / 2;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-
-        img {
-            width: 100%;
-            height: 100%;
-            object-fit: contain;
-            object-position: center;
-        }
-
-        .empty-image{
-            /* background-color: green; */
-            height: 100%;
-            width: 100%;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            border: 1px solid ${theme.colors.greyLight};
-            line-height: 1.5;
-            color: ${theme.colors.greySemiDark};
-            border-radius: ${theme.borderRadius.round};
-        }
-    }
 
     .input-fields {
         grid-area: 1 / 2 / -2 / 3;
