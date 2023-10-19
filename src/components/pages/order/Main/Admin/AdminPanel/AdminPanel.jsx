632d815f83de54491de7b0292334fb511677b865
RatanaSenpai <createbysenpai@gmail.com>
2023-10-19 15:49:38 +0200

    🐛 F08: AdminPanel > Fix TextInput height by adding box-sizing

--- src/components/pages/order/Main/Admin/AdminPanel/AdminPanel.jsx
+++ src/components/pages/order/Main/Admin/AdminPanel/AdminPanel.jsx
@@ -19,9 +19,11 @@ export default function AdminPanel() {
 }
 
 const AdminPanelStyled = styled.div`
-    height: 250px;
+    height: 240px;
     background: ${theme.colors.white};
     border: 1px solid ${theme.colors.greyLight};
     box-shadow: ${theme.shadows.subtle};
-    padding: 30px 5%;
+    box-sizing: border-box; // ajouter ça sinon ça fait grossir le panel
+    padding: 30px 5%; // ajouter au panel et non dans les form car sinon on va devoir le mettre dans les deux form : AddForm et EditForm
+    // les 5% c'est pour les aligner avec le tab.
 `
