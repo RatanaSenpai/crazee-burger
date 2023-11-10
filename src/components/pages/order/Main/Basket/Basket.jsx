7bbf5eb47beba3052d47fabcf704c36481a81c95
RatanaSenpai <createbysenpai@gmail.com>
2023-11-10 16:35:03 +0100

    ♻️ F10 : BASKET > Add Body

--- src/components/pages/order/Main/Basket/Basket.jsx
+++ src/components/pages/order/Main/Basket/Basket.jsx
@@ -1,14 +1,14 @@
 import styled from "styled-components"
-import { theme } from "../../../../../theme"
 import Total from "./Total"
 import { formatPrice } from "../../../../../utils/maths"
 import Footer from "./Footer"
+import BasketBody from "./BasketBody"
 
 export default function Basket() {
     return (
         <BasketStyled>
             <Total amountToPay={formatPrice(0)} />
-            <div className="body">Body</div>
+            <BasketBody />
             <Footer />
         </BasketStyled>
     )
@@ -18,10 +18,4 @@ const BasketStyled = styled.div`
     background: pink;
     display: flex;
     flex-direction: column;
-
-    .body {
-        flex: 1;
-        background: ${theme.colors.background_white};
-        box-shadow: ${theme.shadows.basket};
-    }
 `
---
+++ src/components/pages/order/Main/Basket/BasketBody.jsx
@@ -0,0 +1,30 @@
+import styled from "styled-components"
+import { theme } from "../../../../../theme"
+
+export default function BasketBody() {
+    return (
+    <BasketBodyStyled>
+        <span className="empty-message">Votre commande est vide.</span>
+    </BasketBodyStyled>
+)
+}
+
+const BasketBodyStyled = styled.div`
+    flex: 1;
+    background: ${theme.colors.background_white};
+    box-shadow: ${theme.shadows.basket};
+
+    .empty-message {
+        display: flex;
+        height: calc(95vh - 10vh - 70px - 70px);
+        text-align: center;
+        flex: 1;
+        justify-content: center;
+        align-items: center;
+        align-self: center;
+        line-height: 2;
+        font-family: ${theme.fonts.family.stylish};
+        font-size: ${theme.fonts.size.P4};
+        color: ${theme.colors.greyBlue};
+    }
+`
