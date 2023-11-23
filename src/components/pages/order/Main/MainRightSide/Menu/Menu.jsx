a758638f327620bf0a86f3556fb186756161160b
RatanaSenpai <createbysenpai@gmail.com>
2023-11-23 16:11:01 +0100

    ✨ F11: MENU > add handleDeleteBasketProduct to handleOnDelete

--- src/components/pages/order/Main/MainRightSide/Menu/Menu.jsx
+++ src/components/pages/order/Main/MainRightSide/Menu/Menu.jsx
@@ -23,6 +23,7 @@ export default function Menu() {
         setCurrentTabSelected,
         titleEditRef,
         handleAddToBasket,
+        handleDeleteBasketProduct
     }= useContext(OrderContext)
     
     // comportements (gestionnaires d'événement ou "event handlers")
@@ -44,6 +45,7 @@ export default function Menu() {
     const handleCardDelete = (event, idProductToDelete) => { 
         event.stopPropagation()
         handleDelete(idProductToDelete)
+        handleDeleteBasketProduct(idProductToDelete)
         idProductToDelete === productSelected.id && setProductSelected(EMPTY_PRODUCT)
         titleEditRef.current.focus()
     }
