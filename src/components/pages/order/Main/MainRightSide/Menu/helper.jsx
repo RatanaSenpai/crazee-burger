a4c50bbea41ea4183f761c68f2e5827c6bae7ff1
RatanaSenpai <createbysenpai@gmail.com>
2024-01-10 14:24:53 +0100

    ✨ F12: TOTAL > Avoid NaN price to mess up with sumToPay

--- src/components/pages/order/Main/MainRightSide/Menu/helper.jsx
+++ src/components/pages/order/Main/MainRightSide/Menu/helper.jsx
@@ -1,9 +1,3 @@
-import { findObjectById } from "../../../../../../utils/array"
-
-export const calculateSumToPay = (basket, menu) => {
-    return basket.reduce((total, basketProduct) => {
-        const menuProduct = findObjectById(basketProduct.id, menu)
-        total += menuProduct.price * basketProduct.quantity
-        return total
-    }, 0)
+export const checkIfProductIsClicked = (idProductInMenu, idProductClickedOn) => {
+    return idProductInMenu === idProductClickedOn
 }
